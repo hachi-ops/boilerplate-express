a05f9cfaa56957fe3a694f11ea7988529931185a
hachi-ops <67706415+hachi-ops@users.noreply.github.com>
2022-05-17 14:40:05 +0000

    step 3

--- myApp.js
+++ myApp.js
@@ -3,12 +3,12 @@ let app = express();
 
 console.log("Hello World")
 
-app.get('/', function(req, res) {
-  res.send('Hello Express');
-}
-)
+// app.get('/', function(req, res) {
+//   res.send('Hello Express');
+// }
+// )
 
-
+app.get('/', _dirname + /views/index.html)
 
 
 
