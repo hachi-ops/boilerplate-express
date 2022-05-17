9a0522c1c79afa69f2a413b6c8b3ce6e33dc1fa2
hachi-ops <67706415+hachi-ops@users.noreply.github.com>
2022-05-17 17:18:32 +0000

    step 7 (or 8 ?)

--- myApp.js
+++ myApp.js
@@ -1,6 +1,12 @@
 let express = require('express');
 let app = express();
 
+
+app.use((req, res, next) => {
+  console.log(req.method + ' ' + req.path + ' - ' + req.ip)
+  next()
+}
+  )
 console.log("Hello World")
 
 // app.get('/', function(req, res) {
